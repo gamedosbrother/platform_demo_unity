dfa0a9acf842633b00d8a2ae40d27f970b001d23
mjrfornari <mjrfornari@gmail.com>
2019-03-19 20:17:20 -0300

    Double Jump implemented

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -18,7 +18,8 @@ public class PlayerMovement : MonoBehaviour
     private float verticalMovement;
     private float horizontalMovement;
     private float distToGround;
-    private bool jumped;
+    private bool hasJumpedLastFrame;
+    private bool hasJumped;
     private bool firstJump;
     private bool secondJump;
     
@@ -32,9 +33,9 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
-    bool IsGrounded() {
+    bool IsGrounded() 
+    {
         // Show!
-        // Só não entendi pq não está pegando o Player haha, mas depois vemos isso.
         // Aqui vou explicar sobre Layers pra vcs
         return Physics.Raycast(transform.position, Vector3.down, distToGround + 0.1f);
     }
@@ -45,47 +46,43 @@ public class PlayerMovement : MonoBehaviour
         horizontalMovement = Input.GetAxisRaw("Horizontal");
         // Evitem usar string para os GetKey, usem os KeyCode
         if (Input.GetKeyDown(KeyCode.Space)) {
-            jumped = true;
+            hasJumpedLastFrame = true;
         }
 
     }
 
+    void Jump() 
+    {
+        Vector3 velocity = rigidbody.velocity;
+        velocity.y = jumpForce;
+        rigidbody.velocity = velocity;
+    }
+
     void FixedUpdate()
     {
-        // Certinho o que tinha que ser feito, mas, sugiro trocar por Vector3.down * rigidbody.velocity.y
-        rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, 0) + verticalMovement * moveSpeed * transform.forward;
+        bool isGrounded = IsGrounded();
+
+        rigidbody.velocity = Vector3.up * rigidbody.velocity.y + verticalMovement * moveSpeed * transform.forward;
         rigidbody.MoveRotation(
             Quaternion.Euler(0f, transform.eulerAngles.y + (horizontalMovement * rotationSpeed * Time.fixedDeltaTime), 0f)
         );
-        
-        if (jumped) {
-            jumped = false;
-            Debug.Log("Pulou");
-            
-            if (!secondJump && firstJump){
-                Debug.Log("Pulo2");
-                // Não precisa do Time.fixedDeltaTime aqui. O AddForce adiciona a força e deixa pra física controlar, já que ele não vai alterar todo frame
-                // Só precisa adicionar o valor.
-                //
-                // EU ACHO que fica mais legível se, em vez de mandar 3 parametros, mandar só 1 com Vector3.up * jumpForce
-                //
-                // Centralizar isso para uma função, não precisamos chamar 2 vezes a mesma coisa
-                rigidbody.AddForce(0f, jumpForce * Time.fixedDeltaTime, 0f);
-                secondJump = true;
-            } else if (!firstJump) {
-                Debug.Log("Pulo1");
-                rigidbody.AddForce(0f, jumpForce * Time.fixedDeltaTime, 0f);
-                firstJump = true;
+
+        if (hasJumpedLastFrame)
+        {
+            hasJumpedLastFrame = false;
+            if (isGrounded)
+            {
+                hasJumped = true;
+                Jump();
+            } else if (hasJumped) 
+            {
+                Jump();
+                hasJumped = false;
             }
-          
         }
         
-        if (IsGrounded()) {
-            Debug.Log("Resetou");
-            firstJump = false;
-            secondJump = false;
-            jumped = false;
-        }
+        
+
     }
 
 }
