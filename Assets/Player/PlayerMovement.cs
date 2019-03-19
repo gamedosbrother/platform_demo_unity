1186d1324bbce4deed5b04d21f23d216b3ebfcda
mjrfornari <mjrfornari@gmail.com>
2019-03-19 01:55:06 -0300

    Fall bug fixed

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -9,29 +9,72 @@ public class PlayerMovement : MonoBehaviour
     private float moveSpeed;
     [SerializeField]
     private float rotationSpeed;
+    [SerializeField]
+    private float jumpForce;
 
     private Rigidbody rigidbody;
+    private BoxCollider collider;
 
     private float verticalMovement;
     private float horizontalMovement;
+    private float distToGround;
+    private bool jumped;
+    private bool firstJump;
+    private bool secondJump;
+    
+
 
     void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<BoxCollider>();
+        distToGround = collider.bounds.extents.y;
+
+    }
+
+    bool IsGrounded() {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
     }
 
     void Update()
     {   
         verticalMovement = Input.GetAxisRaw("Vertical");
         horizontalMovement = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown("space")) {
+            jumped = true;
+        }
+
     }
 
     void FixedUpdate()
     {
-        rigidbody.velocity = transform.forward * verticalMovement * moveSpeed;
+        rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, 0) + verticalMovement * moveSpeed * transform.forward;
         rigidbody.MoveRotation(
             Quaternion.Euler(0f, transform.eulerAngles.y + (horizontalMovement * rotationSpeed * Time.fixedDeltaTime), 0f)
         );
+        
+        if (jumped) {
+            jumped = false;
+            Debug.Log("Pulou");
+            
+            if (!secondJump && firstJump){
+                Debug.Log("Pulo2");
+                rigidbody.AddForce(0f, jumpForce * Time.fixedDeltaTime, 0f);
+                secondJump = true;
+            } else if (!firstJump) {
+                Debug.Log("Pulo1");
+                rigidbody.AddForce(0f, jumpForce * Time.fixedDeltaTime, 0f);
+                firstJump = true;
+            }
+          
+        }
+        
+        if (IsGrounded()) {
+            Debug.Log("Resetou");
+            firstJump = false;
+            secondJump = false;
+            jumped = false;
+        }
     }
 
 }
