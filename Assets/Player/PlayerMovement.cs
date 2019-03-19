ab46a59482abeac411dc0394613054a872dd70e4
Artur Morelle Nista <arturnista@gmail.com>
2019-03-19 01:00:39 -0300

    First commit

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float rotationSpeed;
+
+    private Rigidbody rigidbody;
+
+    private float verticalMovement;
+    private float horizontalMovement;
+    private Vector3 velocity;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {   
+        verticalMovement = Input.GetAxisRaw("Vertical");
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.velocity = transform.forward * verticalMovement * moveSpeed;
+        velocity = rigidbody.velocity;
+        rigidbody.MoveRotation(
+            Quaternion.Euler(0f, transform.eulerAngles.y + (horizontalMovement * rotationSpeed * Time.fixedDeltaTime), 0f)
+        );
+    }
+
+}
