3749e96c6062cb47f417ba8859d25767216338da
Artur Morelle Nista <arturnista@gmail.com>
2019-03-20 08:34:49 -0300

    Added firing at blue barrels that I call them enemies but they have goblins sounds

---
+++ Assets/Enemy/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+    [SerializeField]
+    private float moveSpeed = 2f;
+    [SerializeField]
+    private float maxHealth = 100f;
+    [SerializeField]
+    private AudioClip deathSound;
+
+    private AudioSource audioSource;
+
+    private Transform player;
+
+    private float health;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        player = GameObject.FindObjectOfType<PlayerMovement>().transform;
+        health = maxHealth;
+    }
+
+    void Update()
+    {
+        if(health <= 0f) return;
+        transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+    }
+    
+    public void DealDamage(float damage)
+    {
+        if(health <= 0f) return;
+
+        health -= damage;
+        if(health <= 0f)
+        {
+            audioSource.clip = deathSound;
+            audioSource.Play();
+        }
+        else
+        {
+            audioSource.Play();
+        }
+    }
+
+}
---
+++ Assets/Player/PlayerAttack.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+    [SerializeField]
+    private float attackDelay;
+
+    private Camera camera;
+    private AudioSource audioSource;
+
+    private bool isAttacking;
+
+    void Awake()
+    {
+        camera = Camera.main;    
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(AttackCycle());
+    }
+
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            isAttacking = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            isAttacking = false;
+        }
+
+    }
+
+    IEnumerator AttackCycle()
+    {
+        while (true)
+        {
+            if(isAttacking) Attack();
+            yield return new WaitForSeconds(attackDelay);
+        }
+    }
+
+    void Attack()
+    {
+        audioSource.Play();
+        Debug.DrawRay(transform.position, transform.forward * 10f, Color.red, attackDelay * .9f);
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if(enemy != null)
+            {
+                enemy.DealDamage(10f);
+            }
+        }
+    }
+
+}
